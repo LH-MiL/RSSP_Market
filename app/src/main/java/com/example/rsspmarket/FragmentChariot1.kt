cf3828ebe1d8c33c15a0354eee06dd0136720689
user <lahmaim@gmail.com>
2023-05-17 19:19:06 +0100

    S11

--- app/src/main/java/com/example/rsspmarket/FragmentChariot1.kt
+++ app/src/main/java/com/example/rsspmarket/FragmentChariot1.kt
@@ -8,6 +8,8 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 
 class FragmentChariot1: Fragment(R.layout.fragment_chariot1){
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_chariot1, container, false)
         //Le corps de la fonction
